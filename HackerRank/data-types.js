d30f61cb20bffe6cf3d65a1de2da7bd34ee0cc52
silviatofana <silvia.tofana@gmail.com>
2023-01-09 21:30:30 +0200

    data-types solution

---
+++ HackerRank/data-types.js
@@ -0,0 +1,22 @@
+    // Declare second integer, double, and String variables.  
+    let secondInteger;
+    let secondDouble;
+    let secondString;
+
+    // Read and save an integer, double, and String to your variables.
+    let data = input.split('\n');
+
+    integer = Number(readLine());
+    float = Number(readLine())
+    string = Number(readLine());
+
+    // Print the sum of both integer variables on a new line.
+    console.log(secondInteger + i);
+
+    // Print the sum of the double variables on a new line.
+    console.log((secondDouble + d).toFixed(1));
+
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    console.log(`${s}${secondString}`) 
+    
