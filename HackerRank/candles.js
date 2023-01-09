e2ff5de79b0f9a3b72f55be58df5e02e69986293
silviatofana <silvia.tofana@gmail.com>
2023-01-09 21:29:57 +0200

    candles solution

---
+++ HackerRank/candles.js
@@ -0,0 +1,52 @@
+// function birthdayCakeCandles(candles) {
+//   // Write your code here
+//   const t0 = performance.now();
+
+//   let maxValue = candles[0];
+//   let maxValueCount = 0;
+
+//   candles.map((candle) => {
+//     if (candle > maxValue) {
+//       maxValue = candle;
+//       maxValueCount = 1;
+//     } else if (candle === maxValue) {
+//       maxValueCount++;
+//     }
+//   });
+
+//   const t1 = performance.now();
+//   console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
+
+//   return maxValueCount;
+
+
+// }
+// // 1 Million random numbers in an array
+// // new Array(1000000).fill(0).map((_, i) => i);
+
+// console.log(birthdayCakeCandles(new Array(10000000).fill(0).map(() => Math.floor(Math.random() * 10000000))));
+
+
+function birthdayCakeCandles(candles) {
+  // Write your code here
+  const t0 = performance.now();
+
+  const maxValue = candles.reduce((a, b) => { return Math.max(a, b) }); 
+  let maxValueCount = 0;
+
+  for (let i = 0; i < candles.length; i++) {
+    if (candles[i] === maxValue) {
+      maxValueCount += 1;
+    }
+  }
+
+  const t1 = performance.now(); 
+  console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
+
+  return maxValueCount;
+
+}
+// 1 Million random numbers in an array
+// new Array(1000000).fill(0).map((_, i) => i);
+
+console.log(birthdayCakeCandles(new Array(10000000).fill(0).map(() => Math.floor(Math.random() * 10000000))));
