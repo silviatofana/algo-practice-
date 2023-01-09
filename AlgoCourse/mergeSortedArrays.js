dccf30c013a0cfe1e90e401f89fcab0e3374f86b
silviatofana <silvia.tofana@gmail.com>
2023-01-09 21:23:59 +0200

    mergeSortedArrays solution

---
+++ AlgoCourse/mergeSortedArrays.js
@@ -0,0 +1,43 @@
+// mergeSortedArrays([0,3,4,31], [4,6,30]);
+// // [ 0, 3, 4, 4, 6, 30, 31 ]
+//
+// function mergeSortedArrays(arr1, arr2) {
+//     return [...arr1, ...arr2].sort((a,b) => a -b )
+// }
+//
+// console.log(mergeSortedArrays([0,3,4,31], [4,6,30]));
+
+function mergeSortedArrays(array1, array2) {
+    const mergedArray = [];
+    let array1Item = array1[0];
+    let array2Item = array2[0];
+    let i = 1;
+    let j = 1;
+
+    // check input
+    if (array1.length === 0) {
+        return array2;
+    }
+
+    if (array2.length === 0) {
+        return array1;
+    }
+
+    while (array1Item || array2Item) {
+        console.log(array1Item, array2Item)
+        if (!array2Item || array1Item < array2Item) {
+            mergedArray.push(array1Item);
+            array1Item = array1[i];
+            i++;
+        } else {
+            mergedArray.push(array2Item);
+            array2Item = array2[j];
+            j++;
+        }
+    }
+    console.log(mergedArray)
+    return mergedArray;
+}
+
+
+mergeSortedArrays([0,3,4,31], [4,6,30])
