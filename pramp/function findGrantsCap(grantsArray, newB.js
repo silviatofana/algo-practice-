18a88d479f02b576a951fb40e0f6219fca171b78
silviatofana <silvia.tofana@gmail.com>
2023-02-10 08:02:38 +0200

    function findGrantsCap(grantsArray, newBudget)

---
+++ pramp/function findGrantsCap(grantsArray, newB.js
@@ -0,0 +1,13 @@
+function findGrantsCap(grantsArray, newBudget) {
+    grantsArray.sort((a, b) => a - b); // sort the grantsArray in ascending order
+    let n = grantsArray.length;
+    let total = 0;
+    for (let i = 0; i < n; i++) {
+        if (grantsArray[i] * (n - i) <= newBudget - total) {
+            total += grantsArray[i];
+        } else {
+            return (newBudget - total) / (n - i);
+        }
+    }
+    return grantsArray[n - 1];
+}
