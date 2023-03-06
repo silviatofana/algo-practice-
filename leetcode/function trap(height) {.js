e4bbf573b3287e21e4bc202163b7eeec7a70d347
silviatofana <silvia.tofana@gmail.com>
2023-03-06 11:33:07 +0200

    function trap(height)

---
+++ leetcode/function trap(height) {.js
@@ -0,0 +1,25 @@
+function trap(height) {
+    let left = 0, right = height.length - 1;
+    let leftMax = 0, rightMax = 0;
+    let water = 0;
+    
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                water += leftMax - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                water += rightMax - height[right];
+            }
+            right--;
+        }
+    }
+    
+    return water;
+}
